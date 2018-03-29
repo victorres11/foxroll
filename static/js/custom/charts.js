7d6629c281bd571101513cb18a19a663dd410a18
Victor Torres <victor@adroll.com>
2018-03-29 13:52:22 -0700

    Fixed active s3 or csv options, sass for new UI elements

--- static/js/custom/charts.js
+++ static/js/custom/charts.js
@@ -1,10 +1,15 @@
-console.log("Hello from charts.js")
+function makeCsvActive() {
+  var uploadCsvDiv = document.getElementById("uploadCsvFile_div");
+  var uploads3Div = document.getElementById("uploadS3File_div");
 
-function switchDisplayForm() {
-  var uploadDiv = document.getElementById("uploadFile_div");
-  if (uploadDiv.style.display === "none") {
-      uploadDiv.style.display = "block";
-  } else {
-      uploadDiv.style.display = "none";
-  }
+  uploadCsvDiv.style.display = "block"
+  uploads3Div.style.display = "none"
+}
+
+function makeS3Active() {
+  var uploadCsvDiv = document.getElementById("uploadCsvFile_div");
+  var uploads3Div = document.getElementById("uploadS3File_div");
+
+  uploadCsvDiv.style.display = "none"
+  uploads3Div.style.display = "block"
 }
