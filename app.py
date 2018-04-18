6fdc07030e7d38f26723196958eaeb1a1ce6317a
Victor Torres <victor@adroll.com>
2018-04-18 10:34:48 -0700

    Turn off debug mode in prod, add error pages

--- app.py
+++ app.py
@@ -212,6 +212,15 @@ def get_results(job_key):
     else:
         return "Nay!", 202
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404-alt.html'), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500-alt.html'), 500
+
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=False)
